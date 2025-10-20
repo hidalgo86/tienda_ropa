deb09a889821da9586a020b726d33d8bc0b0b371
Hidalgo <hidalgojose86@gmail.com>
2025-10-20 22:15:37 +0200

    .

--- app/components/ReduxProvider.tsx
+++ app/components/ReduxProvider.tsx
@@ -5,7 +5,7 @@ import { store } from "@/store";
 
 export default function ReduxProvider({
   children,
-}: {
+}: { 
   children: React.ReactNode;
 }) {
   return <Provider store={store}>{children}</Provider>;
